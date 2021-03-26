035b7baa081ff2fb42a1ac7d5ef134254c8693f1
Rodrigo Mendez Gamboa <rodrigo.mendez@infolob.com>
2021-03-26 16:51:19 -0600

    Added hello world

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Rodrigo Méndez</p>
-        <p>Hello World</p>
+        <p>Hello World 2</p>
         <p>{count}</p>
         <button onClick={handleIncrease}>+</button>
         <button onClick={handleDecrease}>-</button>
