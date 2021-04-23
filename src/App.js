8b46642b81ce66e9989f345c28a8213df81f87c9
Rodrigo Mendez Gamboa <rodrigo.mendez@infolob.com>
2021-04-23 11:36:05 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [count, setCount] = useState(0);
 
   const handleIncrease = () => setCount(count + 1);
   const handleDecrease = () => setCount(count - 1);
